79293f214dca7dd13bfac7437933d2df37fbdc46
Kat Phelps <kphelps2@andrew.cmu.edu>
2020-05-04 20:36:58 -0400

    done

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,12 +1,51 @@
-// Activate Carousel
-$("#myCarousel").carousel();
+// CAROUSEL FOR TRES
+$("#my-carousel").carousel();
 
 // Enable Carousel Indicators
 $(".item").click(function(){
-  $("#myCarousel").carousel(1);
+  $("#my-carousel").carousel(1);
 });
 
 // Enable Carousel Controls
 $(".left").click(function(){
-  $("#myCarousel").carousel("prev");
+  $("#my-carousel").carousel("prev");
+});
+
+// CAROUSEL 1
+$("#quatro-carousel-1").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+  $("#quatro-carousel-1").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+  $("#quatro-carousel-1").carousel("prev");
+});
+
+// CAROUSEL 2
+$("#quatro-carousel-2").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+  $("#quatro-carousel-2").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+  $("#quatro-carousel-2").carousel("prev");
+});
+
+// CAROUSEL 3
+$("#quatro-carousel-3").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+  $("#quatro-carousel-3").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+  $("#quatro-carousel-3").carousel("prev");
 });
